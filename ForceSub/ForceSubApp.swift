43a9cd83ea98ee98ff71e54b184e48496a1cf717
Dmitriy Khvan <dmitriy.khvan@tesco.com>
2026-02-14 16:02:31 +0000

    Initial Commit

---
+++ ForceSub/ForceSubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForceSubApp.swift
+//  ForceSub
+//
+//  Created by Dmitriy Khvan on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ForceSubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
